cab73795b55b38875404633220b072baf58ace45
Alejandro Gordillo <a.gordillor@uniandes.edu.co>
2025-11-16 13:52:48 -0500

    spring

---
+++ alpescab/alpescab/src/main/java/uniandes/edu/co/alpescab/AlpescabApplication.java
@@ -0,0 +1,13 @@
+package uniandes.edu.co.alpescab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpescabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpescabApplication.class, args);
+	}
+
+}
